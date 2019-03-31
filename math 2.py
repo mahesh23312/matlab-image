d191d26239af9eecd71ac2e8b22be89c2179116d
mahesh23312 <mahesh23312@gmail.com>
2019-03-31 18:21:09 +0530

    test commit

---
+++ math 2.py
@@ -0,0 +1,13 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=b
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a=b
+NameError: name 'b' is not defined
+>>> a=7
+>>> b=6
+>>> c=a*b
+>>> print(c)
+42
+>>> 
---
+++ math ex.py
@@ -0,0 +1,10 @@
+a=9
+b=6
+c=a+b
+d=a*b
+e=a-b
+f=a/b
+print(c)
+print(d)
+print(e)
+print(f)
